c53e8ec98c75362271a98a07b4fedd7f16a0c9c6
Ádrian Lívio Vasconcelos Guedes <nhdezoito@gmail.com>
2017-09-22 09:44:42 -0300

    Added build script to clang image

--- clang/build.sh
+++ clang/build.sh
@@ -4,7 +4,7 @@ TAG=nhdezoito/clang:latest
 
 #docker pull ${TAG}
 #docker build --rm -f clang/Dockerfile -t ${TAG} clang
-echo ${DOCKER_PASSWD}
-echo ${DOCKER_USER}
+echo "${DOCKER_PASSWD}"
+echo "${DOCKER_USER}"
 docker login --username "${DOCKER_USER}" --password "${DOCKER_PASSWD}"
 #docker push ${TAG}
