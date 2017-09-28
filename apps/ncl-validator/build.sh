6447287fc79d981e697f42d798d93ae514b08d4f
Ádrian Lívio Vasconcelos Guedes <nhdezoito@gmail.com>
2017-09-28 09:48:43 -0300

    Added tag

--- apps/ncl-validator/build.sh
+++ apps/ncl-validator/build.sh
@@ -10,7 +10,7 @@ cd "${dir}"
 
 # Build using build image and source from github
 mkdir -p ${dir}/build
-git clone --depth=1 https://github.com/TeleMidia/nclcomposer.git "${dir}/nclcomposer"|| true
+git clone https://github.com/TeleMidia/nclcomposer.git "${dir}/nclcomposer"|| true
 
 cmd_generate="cd ${dir}/build && pwd && ls -lah ${dir}/nclcomposer && cmake -DWITH_VALIDATOR_EXE=ON ${dir}/nclcomposer"
 docker run -i --rm -v ${dir}:${dir} ${build_image} bash -c "${cmd_generate}"
@@ -18,7 +18,14 @@ cmd_build="cd ${dir}/build && nproc && cmake --build . --target all -- -j$(nproc
 docker run -i --rm -v ${dir}:${dir} ${build_image} bash -c "${cmd_build}"
 
 # Create app docker image
+docker pull ${app_image}
 docker build --rm -f "${dir}/Dockerfile" -t "${app_image}" "${dir}"
 docker run -i --rm ${app_image}
 docker login --username "${DOCKER_USER}" --password "${DOCKER_PASSWD}"
 docker push ${app_image}
+
+cd ${dir}/nclcomposer
+latest_commit=$(git describe --always)
+latest_tag="nhdezoito/ncl-validator:${latest_commit}"
+docker tag ${app_image} ${latest_tag}
+docker push ${latest_tag}
