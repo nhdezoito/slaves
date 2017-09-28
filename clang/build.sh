1cd515be9ee7ea78f852aa4682816eb3de71b99f
Ádrian Lívio Vasconcelos Guedes <nhdezoito@gmail.com>
2017-09-28 08:58:30 -0300

    move location of clang slave

--- clang/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TAG=nhdezoito/clang:latest
-
-docker pull ${TAG}
-docker build --rm -f clang/Dockerfile -t ${TAG} clang
-docker login --username "${DOCKER_USER}" --password "${DOCKER_PASSWD}"
-docker push ${TAG}
